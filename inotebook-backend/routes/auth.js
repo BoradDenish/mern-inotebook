2a7d34f8377fe349cde8a1f31029e5cb8b3762cd
Denish Borad <denishgithub4253@gmail.com>
2025-02-12 09:00:05 +0530

    working on validation

--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import User from '../models/Users.js';
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
 // Create a user using: POST "/api/auth/". Doesn't require Auth
-router.post('/', async (req, res) => {
+router.post('/', [
+    body('name').isLength({min: 3}),
+    body('email').isEmail(),
+    body('password').isLength({min: 5})
+], async (req, res) => {
     try {
         console.log(req.body);
         const user = new User(req.body);
