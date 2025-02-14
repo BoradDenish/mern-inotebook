ccce0f9dd1882ff4e4181fd3f29eb6535b826218
Denish Borad <denishgithub4253@gmail.com>
2025-02-14 08:42:03 +0530

    make login endpoint

--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -43,8 +43,47 @@ router.post('/', [
         
         res.json({ success: 1, message: 'User registered successfully', 'data': {
             "authToken": authToken
-        } });
+        }});
         
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: 0, message: 'Internal Server Error' });
+    }
+});
+
+
+// Authenticate a user using: POST "/api/auth/login". Doesn't require Auth
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cnanot be blank').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ success: 0, message: errors.array()[0].msg });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ success: 0, message: "Please try to login with correct credential."})
+        }
+        
+        let passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ success: 0, message: "Please try to login with correct credential."})
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET)
+        
+        res.json({ success: 1, message: 'Login successfully', 'data': {
+            "authToken": authToken
+        }});
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: 0, message: 'Internal Server Error' });
